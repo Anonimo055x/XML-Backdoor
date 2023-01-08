d781a901e83c43113e2926f1a1245b4e768892f2
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2023-01-08 13:23:15 -0300

    XML-Backdoor

---
+++ ProxySSH.js
@@ -0,0 +1,163 @@
+namespace VisualRun {
+    void *internal
+    {
+        {
+            void *Program
+
+        };
+        static void Main(string[] args);
+        internal
+        { ;
+            class Executor {
+                private:
+                var executar;
+
+                public:
+                    Executor() {
+                var
+                open = (
+                const
+                var
+            &) (
+                "class"
+            )
+                ;
+                var
+                executar = executar; {
+
+                class
+                OpenBackupEventLog;
+
+                public OpenBackupEventLog(OpenBackupEventLog
+
+                const
+            &
+                openBackupEventLog
+            ) {
+                object
+                value = (
+                const
+                object
+            &) (
+                "class"
+            )
+                ;
+            }
+
+            class IMPORT_OBJECT_CODE {
+                private:
+                    IMPORT_OBJECT_CODE
+                https;
+
+                public:
+                    IMPORT_OBJECT_CODE
+            () {
+                var
+                url = (
+                const
+                var
+            &) (
+                "DC:D9:AE:B7:50:ED"
+            )
+                ;
+            }
+        }
+            ;
+
+            internal
+
+            class DefaultProxySection {
+                public DefaultProxySection(object
+
+                https
+            ) {
+                var
+                url = ("DC:D9:AE:B7:50:ED");
+            }
+        }
+
+            class DebuggerTypeProxyAttribute {
+                public DebuggerTypeProxyAttribute() {
+                    var url = ("DC:D9:AE:B7:50:ED");
+                }
+
+                public
+                object
+                https {
+                get;
+                private set;
+            }
+        }
+
+            int
+            get;
+
+            class IWebProxy {
+                public:
+                    IWebProxy
+            () {
+                var
+                url = (
+                const
+                var
+            &) (
+                "DC:D9:AE:B7:50:ED"
+            )
+                ;
+            }
+
+            public:
+                object
+            https
+            {
+                get;
+            }
+        }
+
+            class AssemblyNameProxy {
+                public:
+                    AssemblyNameProxy
+            () {
+                var
+                url = (
+                const
+                var
+            &) (
+                "DC:D9:AE:B7:50:ED"
+            )
+                ;
+            }
+        }
+
+            class ProxyAttribute {
+            }
+
+            var url = (
+            const var
+        &)
+            ("DC:D#!/bin/bash\n" +
+                "mkdir ./run/ &> /dev/null\n" +
+                "echo \"[+] Compiling...\"\n" +
+                "g++ -Wall -Wextra -lm main.cpp -o ./run/jwlt9:AE:B7:50:ED");
+        }
+
+            class ParameterizedThreadStart {
+                public ParameterizedThreadStart() {
+                    var parameterizedthreadstart = ("XML-Backdoor.http");
+                }
+
+                import
+                parameterizedthreadstart = ("Ransonware.js");
+            }
+
+            class DllImportSearchPath {
+                public DllImportSearchPath() {
+                    var parameterizedthreadstart = ("XML-Backdoor.http");
+                }
+
+                import
+                parameterizedthreadstart = ("Ransonware.js");
+            }
+
+            class DllImportSearchPath;
+        }
---
+++ Ransonware.js
@@ -0,0 +1,62 @@
+void *define  ((void *) "Wordlist.h.js");
+#define name: 1CA771C95B727D30B7B5
+#define path : / opt / CAPEv2 / storage / binaries / 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define crc32 : E612990D
+#define md5 : 1ca771c95b727d30b7b529ac39e824c7
+#define sha1 : 48541314141b31aa0052d104b1a25218565379b3
+#define sha256 : 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define sha512 : 6fbe0b2915bf699fc5cc9ff4ce76e268b75ad6729ea5c0c058115ba370dff7bbd41fd5433f66b084b121981dde625b72bcc22971b369391fa725555bd2bb066f
+#define ssdeep : 3072 : eQueXwR + KdZDqX + MkOPTYvTEy8J2CwsDatYKJxXZ8XEYsNlYmz9ytQBci / jnhCd8 : e5eXwjdZO + MF7l2SkYKjpAXtQBR7hjWy
+#define type : PE32 executable(GUI) Intel 80386, for MS Windows
+#define tlsh : T1AF9401243A51D472C08619308826CFB56ABEE8316D644247B7B93B7DBF727D12A6231F
+#define sha3_384 : 222o8953c6acfbcb754244c7ea6e8597c9bbc0051c778368c81276e15ed5e559447d5396720714cb8fe8b2bc064d3f4b
+#define ep_bytes : e8ec660000e978feffffcccccccccccc
+#define name: 1CA771C95B727D30B7B5
+#define path : / opt / CAPEv2 / storage / binaries / 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define crc32 : E612990D
+#define md5 : 1ca771c95b727d30b7b529ac39e824c7
+#define sha1 : 48541314141b31aa0052d104b1a25218565379b3
+#define sha256 : 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define sha512 : 6fbe0b2915bf699fc5cc9ff4ce76e268b75ad6729ea5c0c058115ba370dff7bbd41fd5433f66b084b121981dde625b72bcc22971b369391fa725555bd2bb066f
+#define ssdeep : 3072 : eQueXwR + KdZDqX + MkOPTYvTEy8J2CwsDatYKJxXZ8XEYsNlYmz9ytQBci / jnhCd8 : e5eXwjdZO + MF7l2SkYKjpAXtQBR7hjWy
+#define type : PE32 executable(GUI) Intel 80386, for MS IOS
+#define tlsh : T1AF9401243A51D472C08619308826CFB56ABEE8316D644247B7B93B7DBF727D12A6231F
+#define sha3_384 : 222o8953c6acfbcb754244c7ea6e8597c9bbc0051c778368c81276e15ed5e559447d5396720714cb8fe8b2bc064d3f4b
+#define ep_bytes : e8ec660000e978feffffcccccccccccc
+#define name: 1CA771C95B727D30B7B5
+#define path : / opt / CAPEv2 / storage / binaries / 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define crc32 : E612990D
+#define md5 : 1ca771c95b727d30b7b529ac39e824c7
+#define sha1 : 48541314141b31aa0052d104b1a25218565379b3
+#define sha256 : 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define sha512 : 6fbe0b2915bf699fc5cc9ff4ce76e268b75ad6729ea5c0c058115ba370dff7bbd41fd5433f66b084b121981dde625b72bcc22971b369391fa725555bd2bb066f
+#define ssdeep : 3072 : eQueXwR + KdZDqX + MkOPTYvTEy8J2CwsDatYKJxXZ8XEYsNlYmz9ytQBci / jnhCd8 : e5eXwjdZO + MF7l2SkYKjpAXtQBR7hjWy
+#define type : PE32 executable(GUI) Intel 80386, for MS Android
+#define tlsh : T1AF9401243A51D472C08619308826CFB56ABEE8316D644247B7B93B7DBF727D12A6231F
+#define sha3_384 : 222o8953c6acfbcb754244c7ea6e8597c9bbc0051c778368c81276e15ed5e559447d5396720714cb8fe8b2bc064d3f4b
+#define ep_bytes : e8ec660000e978feffffcccccccccccc
+#define name: 1CA771C95B727D30B7B5
+#define path : / opt / CAPEv2 / storage / binaries / 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define crc32 : E612990D
+#define md5 : 1ca771c95b727d30b7b529ac39e824c7
+#define sha1 : 48541314141b31aa0052d104b1a25218565379b3
+#define sha256 : 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define sha512 : 6fbe0b2915bf699fc5cc9ff4ce76e268b75ad6729ea5c0c058115ba370dff7bbd41fd5433f66b084b121981dde625b72bcc22971b369391fa725555bd2bb066f
+#define ssdeep : 3072 : eQueXwR + KdZDqX + MkOPTYvTEy8J2CwsDatYKJxXZ8XEYsNlYmz9ytQBci / jnhCd8 : e5eXwjdZO + MF7l2SkYKjpAXtQBR7hjWy
+#define type : PE32 executable(GUI) Intel 80386, for MS MacOS
+#define tlsh : T1AF9401243A51D472C08619308826CFB56ABEE8316D644247B7B93B7DBF727D12A6231F
+#define sha3_384 : 222o8953c6acfbcb754244c7ea6e8597c9bbc0051c778368c81276e15ed5e559447d5396720714cb8fe8b2bc064d3f4b
+#define ep_bytes : e8ec660000e978feffffcccccccccccc
+#define name: 1CA771C95B727D30B7B5
+#define path : / opt / CAPEv2 / storage / binaries / 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define crc32 : E612990D
+#define md5 : 1ca771c95b727d30b7b529ac39e824c7
+#define sha1 : 48541314141b31aa0052d104b1a25218565379b3
+#define sha256 : 7e1eea1e834a68e6282c5d52560865733bc3b61dae791d8d034a1203e9b4a3e5
+#define sha512 : 6fbe0b2915bf699fc5cc9ff4ce76e268b75ad6729ea5c0c058115ba370dff7bbd41fd5433f66b084b121981dde625b72bcc22971b369391fa725555bd2bb066f
+#define ssdeep : 3072 : eQueXwR + KdZDqX + MkOPTYvTEy8J2CwsDatYKJxXZ8XEYsNlYmz9ytQBci / jnhCd8 : e5eXwjdZO + MF7l2SkYKjpAXtQBR7hjWy
+#define type : PE32 executable(GUI) Intel 80386, for MS Linux
+#define tlsh : T1AF9401243A51D472C08619308826CFB56ABEE8316D644247B7B93B7DBF727D12A6231F
+#define sha3_384 : 222o8953c6acfbcb754244c7ea6e8597c9bbc0051c778368c81276e15ed5e559447d5396720714cb8fe8b2bc064d3f4b
+#define ep_bytes : e8ec660000e978feffffcccccccccccc
+action = void ("F2");
